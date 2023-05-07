5253e2bca9bf655049d8780166c903901a39e584
avedineev <avedineev69@gmail.com>
2023-05-08 00:21:45 +0500

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+
+
+def user_interaction():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    user_interaction()
---
+++ src/super_job.py
@@ -0,0 +1,7 @@
+import json
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('SJ_SECRET_KEY')
